a66767be523acc1428e408ae1dfeaa1708eae854
MeinLiX <meinlix@ukr.net>
2021-07-05 19:43:16 +0300

    Add a basic solution that includes an SQLiteBenchmark project

---
+++ SQLiteBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQLiteBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
