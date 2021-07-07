9c99eb912b087f15605b8f086074916ed4866b0f
MeinLiX <meinlix@ukr.net>
2021-07-07 17:53:57 +0300

    Adapt the newly created library
    Need to use BenchmarkNET new library functionality in the MySQLBenchmark and SQLiteBenchmark projects.

--- MySQLBenchmark/Program.cs
+++ MySQLBenchmark/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using MySql.Data.MySqlClient;
+using BenchmarkNET;
+using BenchmarkNET.RelationSQL;
 
 namespace MySqlBenchmark
 {
     static class Program
     {
         private const string connectionString = "server=localhost;user=root;database=usersdata;password=password;";
-        private static MySqlConnection _mySqlConnection;
+        private static readonly MySqlConnection _mySqlConnection;
         private static List<string> _namesList = new();
-
-        #region sqlExpressions
-        private static string CreateTableUsers() => "CREATE TABLE `usersdata`.`users` (`id` INT NOT NULL AUTO_INCREMENT,`Name` VARCHAR(45) NOT NULL,PRIMARY KEY(`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);";
-        private static string InsertUserToUsersTable(string name) => $"INSERT INTO Users(Name) VALUES('{name}')";
-        private static string UpdateUserFromUsersTable(string oldName, string newName) => $"UPDATE Users SET name='{newName}' WHERE Name='{oldName}'";
-        private static string DeleteUserFromUsersTable(string name) => $"DELETE FROM Users WHERE Name='{name}'";
-        #endregion
 
         static Program()
         {
@@ -27,60 +21,21 @@ namespace MySqlBenchmark
         {
             try
             {
-                _namesList = GenerateNames(100);
+                _namesList = MySqlBanch.GenerateNames(100);
 
                 _mySqlConnection.Open();
-                
+
                 {
-                    Benchmark("InsertUser", () => { _namesList.ForEach(u => _mySqlConnection.CustomExecute(InsertUserToUsersTable(u))); });
-                    Benchmark("DeleteUser", () => { _namesList.ForEach(u => _mySqlConnection.CustomExecute(DeleteUserFromUsersTable(u))); });
+                    BenchmarK.TimeCheck("InsertUser", () => { _namesList.ForEach(u => MySqlBanch.CustomExecute<MySqlCommand>(_mySqlConnection, SQLiteBanch.InsertUserToUsersTable(u))); });
+                    BenchmarK.TimeCheck("DeleteUser", () => { _namesList.ForEach(u => MySqlBanch.CustomExecute<MySqlCommand>(_mySqlConnection, SQLiteBanch.DeleteUserFromUsersTable(u))); });
                 }
 
                 _mySqlConnection.Close();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-        }
-
-        private static int CustomExecute(this MySqlConnection connection, string sqlExpression)
-        {
-            try
-            {
-                _ = connection ?? throw new ArgumentNullException(nameof(connection));
-                _ = sqlExpression ?? throw new ArgumentNullException(nameof(sqlExpression));
-
-                MySqlCommand command = new()
-                {
-                    Connection = connection,
-                    CommandText = sqlExpression
-                };
-                return command.ExecuteNonQuery();
-            }
-            catch { throw; }
-        }
-
-        public static void Benchmark(string Name, Action act)
-        {
-            GC.Collect();
-            Stopwatch sw = Stopwatch.StartNew();
-
-            act.Invoke();
-
-            sw.Stop();
-            Console.WriteLine($"{Name}: {sw.ElapsedMilliseconds} ms");
-        }
-
-        public static List<string> GenerateNames(int count = 1_000)
-        {
-            List<string> returnList = new();
-            Random random = new();
-            while (count-- > 0)
-            {
-                returnList.Add($"{random.Next(1_000_000)}_fakeUser_{random.Next(1_000_000)}");
-            }
-            return returnList;
         }
     }
 }
--- SQLiteBenchmark/Program.cs
+++ SQLiteBenchmark/Program.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
+using BenchmarkNET;
+using BenchmarkNET.RelationSQL;
 
 namespace SQLiteBenchmarks
 {
-
     static class Program
     {
         private const string connectionString = "Data Source=usersdata.db";
-        private static SqliteConnection _sqliteConnection;
+        private static readonly SqliteConnection _sqliteConnection;
         private static List<string> _namesList = new();
-
-        #region sqlExpressions
-        private static string CreateTableUsers() => "CREATE TABLE Users(_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Name TEXT NOT NULL)";
-        private static string InsertUserToUsersTable(string name) => $"INSERT INTO Users(Name) VALUES('{name}')";
-        private static string UpdateUserFromUsersTable(string oldName, string newName) => $"UPDATE Users SET name='{newName}' WHERE Name='{oldName}'";
-        private static string DeleteUserFromUsersTable(string name) => $"DELETE FROM Users WHERE Name='{name}'";
-        #endregion
 
         static Program()
         {
@@ -28,13 +21,13 @@ namespace SQLiteBenchmarks
         {
             try
             {
-                _namesList = GenerateNames(100);
+                _namesList = SQLiteBanch.GenerateNames(100);
 
                 _sqliteConnection.Open();
 
                 {
-                    Benchmark("InsertUser", () => { _namesList.ForEach(u => _sqliteConnection.CustomExecute(InsertUserToUsersTable(u))); });
-                    Benchmark("DeleteUser", () => { _namesList.ForEach(u => _sqliteConnection.CustomExecute(DeleteUserFromUsersTable(u))); });
+                    BenchmarK.TimeCheck("InsertUser", () => { _namesList.ForEach(u => SQLiteBanch.CustomExecute<SqliteCommand>(_sqliteConnection, SQLiteBanch.InsertUserToUsersTable(u))); });
+                    BenchmarK.TimeCheck("DeleteUser", () => { _namesList.ForEach(u => SQLiteBanch.CustomExecute<SqliteCommand>(_sqliteConnection, SQLiteBanch.DeleteUserFromUsersTable(u))); });
                 }
 
                 _sqliteConnection.Close();
@@ -44,45 +37,5 @@ namespace SQLiteBenchmarks
                 Console.WriteLine(e.Message);
             }
         }
-
-        private static int CustomExecute(this SqliteConnection connection, string sqlExpression)
-        {
-            try
-            {
-                _ = connection ?? throw new ArgumentNullException(nameof(connection));
-                _ = sqlExpression ?? throw new ArgumentNullException(nameof(sqlExpression));
-
-                SqliteCommand command = new()
-                {
-                    Connection = connection,
-                    CommandText = sqlExpression
-                };
-                return command.ExecuteNonQuery();
-            }
-            catch { throw; }
-        }
-
-        public static void Benchmark(string Name, Action act)
-        {
-            GC.Collect();
-            Stopwatch sw = Stopwatch.StartNew();
-
-            act.Invoke();
-
-            sw.Stop();
-            Console.WriteLine($"{Name}: {sw.ElapsedMilliseconds} ms");
-        }
-
-        public static List<string> GenerateNames(int count = 1_000)
-        {
-            List<string> returnList = new();
-            Random random = new();
-            while (count-- > 0)
-            {
-                returnList.Add($"{random.Next(1_000_000)}_fakeUser_{random.Next(1_000_000)}");
-            }
-            return returnList;
-        }
-
     }
 }
