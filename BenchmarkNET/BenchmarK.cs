fc91616d3b9554ac8fd84d0184de60140bbd0a7f
MeinLiX <meinlix@ukr.net>
2021-07-07 17:49:31 +0300

    Add benchmark by time

---
+++ BenchmarkNET/BenchmarK.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BenchmarkNET
+{
+    public static class BenchmarK
+    {
+        public static void TimeCheck(string Name, Action act)
+        {
+            GC.Collect();
+            Stopwatch sw = Stopwatch.StartNew();
+
+            act.Invoke();
+
+            sw.Stop();
+            Console.WriteLine($"{Name}: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
