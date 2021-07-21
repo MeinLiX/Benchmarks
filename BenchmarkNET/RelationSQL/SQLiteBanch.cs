08a03d9c8071a292268242a0b79f4113c02e35c5
MeinLiX <meinlix@ukr.net>
2021-07-21 15:46:53 +0300

    Add transactions for SQLiteBench execute

--- BenchmarkNET/RelationSQL/SQLiteBanch.cs
+++ BenchmarkNET/RelationSQL/SQLiteBanch.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Data.Common;
+
 namespace BenchmarkNET.RelationSQL
 {
     public class SQLiteBanch : BaseSQL
@@ -6,5 +9,29 @@ namespace BenchmarkNET.RelationSQL
         #region sqlExpressions
         public static string CreateTableUsers() => "CREATE TABLE Users(_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Name TEXT NOT NULL)";
         #endregion
+
+        public static new int CustomExecute<T>(DbConnection connection, string sqlExpression) where T : DbCommand, new()
+        {
+            try
+            {
+                _ = connection ?? throw new ArgumentNullException(nameof(connection));
+                _ = sqlExpression ?? throw new ArgumentNullException(nameof(sqlExpression));
+
+                using var transaction = connection.BeginTransaction();
+                T command = new();
+
+                command.Connection = connection;
+                command.Transaction = transaction;
+                command.CommandText = sqlExpression;
+                command.Prepare();
+
+                int rowAffected = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                return rowAffected;
+            }
+            catch { throw; }
+        }
+
     }
 }
